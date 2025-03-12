ca4c4d2432297922e3c3015af00fee823327bf94
kondurusai <kondurusai863@gmail.com>
2025-03-12 10:50:24 +0530

    initial setup eposide-01

---
+++ app.js
@@ -0,0 +1,35 @@
+// const h1 = document.createElement("h1");
+// h1.innerHTML = `
+//       <div>
+//        <h2>I am konduru Sai</h2>
+//        <p>I am learing namaste React by Akshay Saini </p>
+//       </div>
+//       `;
+
+// const root = document.getElementById("root");
+// root.appendChild(h1);
+{
+  /* <div>
+  <div>
+    <h1>I am Konduru Sai</h1>
+    <p>I am Learing ReactJs by Akshay Saini</p>
+  </div>
+  <div>
+    <h1>I am Konduru Sai</h1>
+    <p>I am Learing ReactJs by Akshay Saini</p>
+  </div>
+</div>; */
+}
+const innerDiv = React.createElement("div", {}, [
+  React.createElement("h1", {}, "I am konduru sai"),
+  React.createElement("p", {}, "I am Learning ReactJs by Akshay Saini"),
+]);
+const outerDiv = React.createElement("div", {}, [innerDiv, innerDiv]);
+
+const h1 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "this is Created by Akshay Saini Learning By Konduru Sai"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(outerDiv);
